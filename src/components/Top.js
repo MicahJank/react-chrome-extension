63c9bd1dcd1e32635d05257e1cb8568c711b17b5
MicahJank <MicahJank@users.noreply.github.com>
2020-11-12 19:51:07 -0600

    added Button and header component to the Top.js

--- src/components/Top.js
+++ src/components/Top.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import { Button, Icon, Header } from 'semantic-ui-react';
 
 
 const Top = () => {
 
     return (
-        <div>
-            Top
+        <div className="top-container">
+            <Button circular icon>
+                <Icon name='plus' />
+            </Button>
+            <Header as="h1">Noted</Header>
         </div>
     )
 }
